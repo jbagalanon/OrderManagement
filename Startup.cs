3447b95150269cef4612a8b436139a345d12e796
Joemar <jbagalanon@gmail.com>
2020-03-23 14:15:16 +0800

    Added Bootstrao template and change colors

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using Taste.Data;
+using Taste.DataAccess;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
