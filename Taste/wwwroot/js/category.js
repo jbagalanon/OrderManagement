3903dc9721f853ceb12c15b199d0b1ae205b4656
Joemar <jbagalanon@gmail.com>
2020-08-18 11:44:50 +0800

    modiy index and kayout

--- Taste/wwwroot/js/category.js
+++ Taste/wwwroot/js/category.js
@@ -21,7 +21,7 @@ function loadList() {
                     "data": "id",
                     "render": function (data) {
                         return ` <div class ="text-center">
-                            <a href="/Admin/category/upsert id?=${data}" 
+                            <a href="/Admin/category/upsert?id=${data}" 
                                 class="btn btn-success text-white" style="cursor:pointer; width:100px;">
                                 <i class="far fa-edit"></i>Edit
                                 </a>
