c17449ed4cdc4e58280c9abe2748a5a7de6a9ce6
Joemar <jbagalanon@gmail.com>
2020-08-22 00:14:25 +0800

    Added MenuItem Model

---
+++ Taste.Models/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Taste.Models
+{
+   public class MenuItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Invalid Price")]
+        public double Price { get; set; }
+
+        [DisplayName("Category Name")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        public int FoodTypeId { get; set; }
+        [ForeignKey("FoodTypeId")]
+        public virtual FoodType FoodType { get; set; }
+    }
+}
