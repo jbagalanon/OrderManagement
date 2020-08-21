788f9419c6d9f7ec1b4390813e6b3d399af1481c
Joemar <jbagalanon@gmail.com>
2020-08-21 14:56:23 +0800

    Addind FoodType Js and Controller

---
+++ Taste/Controller/FoodTypeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodTypeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FoodTypeController(IUnitOfWork unitOfWork)
+        {
+
+            _unitOfWork = unitOfWork;
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Json(new {data = _unitOfWork.FoodType.GetAll()});
+        }
+    }
+}
