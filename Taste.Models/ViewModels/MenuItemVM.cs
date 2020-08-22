2cf5f274fb017526540a143d276ed5ee26b25fa0
Joemar <jbagalanon@gmail.com>
2020-08-23 01:41:53 +0800

    Adding Menuitem MVVM

---
+++ Taste.Models/ViewModels/MenuItemVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Taste.Models.ViewModels
+{
+    class MenuItemVM
+    {
+
+        //MenuItem is is the main or primary while cat and food is fk
+        public MenuItem MenuItem { get; set; }
+
+        //Selectlistitem is an mvc rendering, this is use fo dropdown selection
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> FoodTypeList { get; set; }
+    }
+}
