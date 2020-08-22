2794de136436bfe860963548bbbddfa2aae5df9b
Joemar <jbagalanon@gmail.com>
2020-08-23 03:11:40 +0800

    Insert and MenuItem Update Post Done

--- Taste/Pages/Admin/MenuItem/Upsert.cshtml.cs
+++ Taste/Pages/Admin/MenuItem/Upsert.cshtml.cs
@@ -93,7 +93,48 @@ namespace Taste.Pages.Admin.MenuItem
             }
             else
             {
-                _unitOfWork.Category.Update(MenuItemObj);
+                //Edit the menu item if is already in database
+
+                // difference between single model to mvvm
+                var objFromDb = _unitOfWork.MenuItem.Get(MenuItemObj.MenuItem.Id);
+
+                //check and validate the file count of image upload
+                if (files.Count > 0)
+                {
+                    //change userimage filename to string
+                    string fileName = Guid.NewGuid().ToString();
+                    //find upload path which is located form images subdir to menuitems
+                    var uploads = Path.Combine(webRootPath, @"image\menuItems");
+
+                    //once the upload is finish the ext must combine
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    //this is for updating.. Check file in database
+                    var imagePath = Path.Combine(webRootPath, objFromDb.Image.TrimStart('\\'));
+
+                    //then set file condition
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                    //once the upload is finish then combine the file and extension
+                    using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension),
+                        FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    MenuItemObj.MenuItem.Image = @"\image\menuItems" + fileName + extension;
+
+                    _unitOfWork.MenuItem.Remove(MenuItemObj.MenuItem);
+                }
+                else
+                {
+                    //if not uploaded
+                    MenuItemObj.MenuItem.Image = objFromDb.Image;
+                }
+
+                _unitOfWork.MenuItem.Update(MenuItemObj.MenuItem);
             }
 
             _unitOfWork.Save();
