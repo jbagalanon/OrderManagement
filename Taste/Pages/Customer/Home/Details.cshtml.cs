01ffcf56fa10f271e6a52ba1f24b4c16c4f6aa44
Joemar <jbagalanon@gmail.com>
2020-09-01 05:40:52 +0800

    Added Validation Script and controller

--- Taste/Pages/Customer/Home/Details.cshtml.cs
+++ Taste/Pages/Customer/Home/Details.cshtml.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Home
 {
@@ -21,8 +23,8 @@ namespace Taste.Pages.Customer.Home
             _unitOfWork = unitOfWork;
         }
 
-        [BindProperty]
-        public ShoppingCart ShoppingCartObj { get; set; }
+        [BindProperty] public ShoppingCart ShoppingCartObj { get; set; }
+
         public void OnGet(int id)
         {
             ShoppingCartObj = new ShoppingCart()
@@ -43,7 +45,40 @@ namespace Taste.Pages.Customer.Home
 
                 ShoppingCartObj.ApplicationUserId = claim.Value;
 
+                ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(s =>
+                    s.ApplicationUserId == ShoppingCartObj.ApplicationUserId
+                    && s.MenuItemId == ShoppingCartObj.MenuItemId);
+
+                if (cartFromDb == null)
+                {
+                    _unitOfWork.ShoppingCart.Add(ShoppingCartObj);
+                }
+                else
+                {
+                   _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, ShoppingCartObj.Count);
+                }
+
+                _unitOfWork.Save();
+
+                var count = _unitOfWork.ShoppingCart
+                    .GetAll(c => c.ApplicationUserId == ShoppingCartObj.ApplicationUserId)
+                    .ToList().Count;
+
+                HttpContext.Session.SetInt32(SD.ShoppingCart, count);
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ShoppingCartObj.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(
+                    includeProperties: "Category,FoodType",
+                    filter: c => c.Id == ShoppingCartObj.MenuItemId);
+                
+                return Page();
+
             }
         }
+
+        
     }
 }
