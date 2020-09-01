0d4323ff62f5a0e1110326d12d6a18e839a304f6
Joemar <jbagalanon@gmail.com>
2020-09-02 04:28:50 +0800

    Finishing Add minus remove in index controller

--- Taste/Pages/Customer/Cart/Index.cshtml.cs
+++ Taste/Pages/Customer/Cart/Index.cshtml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
 using Taste.Models.ViewModels;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Cart
 {
@@ -54,5 +56,60 @@ namespace Taste.Pages.Customer.Cart
                 OrderDetailsCartVM.OrderHeader.OrderTotal += cartList.MenuItem.Price;
             }
         }
+
+        public IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+
+
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostMinus(int cartId)
+        {
+            
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+
+            //check if value to one so that it can be remove
+
+            if (cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+
+                //update session
+                var cnt = _unitOfWork.ShoppingCart.GetAll(
+                    u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+                //setting session and load it automatically
+                HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+                _unitOfWork.Save();
+            }
+
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+
+            //then reset session
+            var cnt = _unitOfWork.ShoppingCart.GetAll(
+                u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+            //setting session and load it automatically
+            HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+
+
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+
     }
 }
