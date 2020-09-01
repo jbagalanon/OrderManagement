2942168f9cf3d494c2d185f542910349784990e8
Joemar <jbagalanon@gmail.com>
2020-09-02 01:02:19 +0800

    Added oordetailsdetails car and cart index

---
+++ Taste.Models/ViewModels/OrderDetailsCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taste.Models.ViewModels
+{
+    public class OrderDetailsCart
+    {
+
+        //list the item from shopping cart directory
+        public List<ShoppingCart>listCart { get; set; }
+
+
+        //add value from orderheader
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
