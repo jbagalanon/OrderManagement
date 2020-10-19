4dcd27733494803e772ed77cf9fe522a974ca8e7
Joemar <jbagalanon@gmail.com>
2020-10-20 02:38:00 +0800

    Modify Layout Page and add order controller

---
+++ Taste/Controller/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Taste.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
